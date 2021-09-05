9c5f29d324db839cb8c56a1eec8aecdc1da8c8b3
sumit chahar <chahar.sumit888@gmail.com>
2021-09-05 17:39:47 +0530

    Complete assignment level 2

--- block-BJaaco/code/index.js
+++ block-BJaaco/code/index.js
@@ -3,10 +3,34 @@ let numbers = [6, 8, 10, 12, 43, 56, 98];
 let userIds = [1230, 234, 1278, 984, 763, 900];
 
 // 1. Add all the values of numbers and userIds array into the new newly created array named `collection`
+let collection = numbers + userIds;
+
 
 // 2. Add all the even numbers from both arrays numbers and userIds into a newly created array named `evenCollection`
+let evenCollection = [];
+for(number of numbers){
+  if(number % 2 == 0){
+    evenCollection.push(number);
+  }
+}
+for(userId of userIds){
+  if(userId % 2 == 0){
+    evenCollection.push(userId);
+  }
+}
 
 // 3. Add all the odd numbers from both arrays numbers and userIds into a newly created array named `oddCollection`
+let oddCollection = [];
+for(number of numbers){
+  if(number % 2 != 0){
+    oddCollection.push(number);
+  }
+}
+for(userId of userIds){
+  if(userId % 2 != 0){
+    oddCollection.push(userId);
+  }
+}
 
 /*
   @param means parameter
@@ -24,8 +48,13 @@ let userIds = [1230, 234, 1278, 984, 763, 900];
     times(5); // ['test', 'test', 'test', 'test', 'test']
 */
 
-function times() {
-  // Your code
+function times(num = 0,str = 'test'){
+  let arr = [];
+  while(num){
+    arr.push(str);
+    num--;
+  }
+  return arr;
 }
 
 // Uncomment the code below and test the output
@@ -48,8 +77,17 @@ function times() {
     revert(['Ryan', 'John', 'Bran']); //['Bran', 'John', 'Ryan']
 */
 
-function revert() {
-  // your code
+
+function revert(arr){
+  return arr.reverse();
+}
+
+function revert1(arr){
+	let result = [];
+	for(item of arr){
+		result.unshift(item);
+	}
+	return result;
 }
 
 // Uncomment the code below and test the output
@@ -70,8 +108,15 @@ function revert() {
     clear(['Ryan', null, 0,  'John', 'Bran']); //['Bran', 'John', 'Ryan']
 */
 
-function clear() {
+function clear(arr) {
   // your code
+  let resArr = [];
+  for(item of arr){
+    if(item){
+      resArr.unshift(item);
+    }
+  }
+  return resArr;
 }
 
 // Uncomment the code below and test the output
@@ -93,8 +138,13 @@ function clear() {
     arrayToObj(['Ryan', 'John']); // {0: 'Ryan', 1: 'John'}
 */
 
-function arrayToObj() {
+function arrayToObj(arr) {
   // your code
+  let obj = {};
+  for(item of arr){
+    obj[arr.indexOf(item)] = item;
+  }
+  return obj;
 }
 
 // Uncomment the code below and test the output
